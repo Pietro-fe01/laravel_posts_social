8a33ddb0cf4d8b779cdbf574a7660491639bca58
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-28 18:00:11 +0100

    Popolata tabella posts tarmite seeder con Faker

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class PostSeeder extends Seeder
 {
@@ -12,8 +15,15 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $new_post = new Post();
+                $new_post->title = $faker->sentence(5);
+                $new_post->image = $faker->imageUrl(400, 400, "social", true, 'post', false);
+                $new_post->description = $faker->text(1500);
+                $new_post->slug = Str::of($new_post->title)->slug('-');
+            $new_post->save();
+        }
     }
 }
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layotus.admin')
+
+@section('content')
+    <h1>Ciao</h1>
+@endsection
